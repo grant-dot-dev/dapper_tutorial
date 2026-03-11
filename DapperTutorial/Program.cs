60bfaa2caae351d0980b79dfe4a6108512db8c83
grant-dot-dev <12433404+grant-dot-dev@users.noreply.github.com>
2026-03-11 21:22:15 +0000

    initial setup

---
+++ DapperTutorial/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var environmentName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Production";
+
+var configuration = new ConfigurationBuilder()
+	.SetBasePath(AppContext.BaseDirectory)
+	.AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+	.AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: false)
+	.AddEnvironmentVariables()
+	.Build();
+
+
+
